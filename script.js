c6c5c025d0c252bd53e699309f0432d02990abf2
Darya-Dubovik <dashka_ivashenko@mail.ru>
2024-11-24 23:50:53 +0300

    hometask-11
    Написала код тренажера "слепой печати"

---
+++ script.js
@@ -0,0 +1,95 @@
+
+const word = document.querySelector(".word");
+//const spans = document.querySelectorAll(".word span");
+const words = ['apple', 'rose', 'moon', 'winner', 'happiness'];
+const correctCount = document.querySelector(".correct-count");
+const wrongCount = document.querySelector(".wrong-count");
+const wordMistakes = document.querySelector(".word-mistakes");
+const timer = document.querySelector("#timer");
+const str = timer.textContent.split(":");
+let seconds = parseInt(str[0]) + parseInt(str[1]);
+
+let timerId = setInterval(() => {
+    seconds++;
+    const minutes = parseInt(str[0]);
+    const remainingSeconds = seconds % 60;
+  
+    timer.textContent = (`${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`);
+    
+    if(seconds >= 60) {
+      clearInterval(timerId);
+      alert('Время закончилось');
+    };
+  }, 1000);
+
+
+function getRandomIndex() {
+    return Math.floor(Math.random() * words.length);
+};
+
+let currentWord = words[getRandomIndex()];
+let error = false;
+
+function renderWord(currentWord) {
+    word.innerHTML = currentWord.split("").map((char) => 
+    `<span>${char}</span>`).join("");
+};
+
+renderWord(currentWord);
+
+let index = 0;
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === currentWord[index]) {
+        isSuccess();
+    } else {
+        isLoss();
+    }
+
+    if (index === currentWord.length) {
+        if (!error) {
+            correctCount.textContent = ++correctCount.textContent;
+        } else {
+            wrongCount.textContent = ++wrongCount.textContent;
+        };
+        
+        isNewWord();
+    }
+    });
+
+function isSuccess() {
+    word.children[index].classList.remove('w');
+    word.children[index].classList.add('c');
+    index++;
+};
+
+function isLoss() {
+    word.children[index].classList.add('w');
+    wordMistakes.textContent = ++wordMistakes.textContent;
+    error=true;
+};
+
+function isNewWord() {
+    checkNumber();
+    currentWord = words[getRandomIndex()];
+    renderWord(currentWord);
+    index=0;
+    error=false;
+};
+
+function checkNumber() {
+    if (correctCount.textContent >= 5) {
+        alert('Вы победили!');
+        correctCount.textContent = 0;
+        wrongCount.textContent = 0;
+        wordMistakes.textContent = 0;
+
+    } else if (wrongCount.textContent >= 5) {
+        alert('Увы..');
+        correctCount.textContent = 0;
+        wrongCount.textContent = 0;
+        wordMistakes.textContent = 0;
+    };
+
+};
+
